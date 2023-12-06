ce1684034f9d280ab2aafe28541b73b2f97aa62e
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-12-06 16:10:56 +0100

    Update Filter.js

--- src/Filter.js
+++ src/Filter.js
@@ -10,14 +10,11 @@ import words from './Words' // list of words
 
 function Filter() {
 
-    let wordLength = 4; // The length of the word we are looking for
-    let includedLetters = ["o", "c"]; //Letters included in the word but whose location is unknown
+    // test data for word "england"
+    let wordLength = 7; // The length of the word we are looking for
+    let includedLetters = ["e", "d"]; //Letters included in the word but whose location is unknown
     let lettersNotIncluded = ["f","r","k"]; // Letters that we know are not included in the word
     let lettersWithLocation = [ // Letters with known exact location
-        {
-            'letter': 'o', 
-            'index':'1'
-        },
         {
             'letter': 'l',
             'index': '3'
