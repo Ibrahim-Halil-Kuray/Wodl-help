9e854435f38bdc312285d39386e795c3600fbf0e
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-12-06 15:55:17 +0100

    Update Filter.js

--- src/Filter.js
+++ src/Filter.js
@@ -11,7 +11,7 @@ import words from './Words' // list of words
 function Filter() {
 
     let wordLength = 4; // The length of the word we are looking for
-    let includedLetters = ["o"]; //Letters included in the word but whose location is unknown
+    let includedLetters = ["o", "c"]; //Letters included in the word but whose location is unknown
     let lettersNotIncluded = ["f","r","k"]; // Letters that we know are not included in the word
     let lettersWithLocation = [ // Letters with known exact location
         {
@@ -29,7 +29,22 @@ function Filter() {
             
     }
     wordsArray = lenFilter(wordsArray, wordLength)
-    
+
+    // filter words containing the letter in the word
+    function containFilter (myArray, containLetters) {
+        return myArray.filter((word)=>{
+        
+            for (let index = 0; index < containLetters.length; index++) {
+                if(word.indexOf(containLetters[index])=== -1){
+                    return false
+                }
+            }
+            return true
+        })
+    }
+
+    if(includedLetters.length > 0) wordsArray = containFilter(wordsArray, includedLetters);
+
   return (
     <div>
         {wordsArray.map((word, index) => {
