e277fd94213b457cbf247e14d0a88efd7ab7fd2f
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-12-06 15:40:13 +0100

    Update Filter.js
    needed variables example

--- src/Filter.js
+++ src/Filter.js
@@ -9,6 +9,17 @@ import React from 'react'
 import words from './Words' // list of words
 
 function Filter() {
+
+    let wordLength = 4; // The length of the word we are looking for
+    let includedLetters = ["o"]; //Letters included in the word but whose location is unknown
+    let lettersNotIncluded = ["f","r","k"]; // Letters that we know are not included in the word
+    let lettersWithLocation = [ // Letters with known exact location
+        {
+            'letter': "o", 
+            'index':"1"
+        }
+    ]
+    
   return (
     <div>
         Result
